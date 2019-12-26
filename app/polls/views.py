43af2c50e7e5a7da91ddf7abe5cdb4294a0ce268
umair.khan <umair.khan@dubizzle.com>
2019-12-26 15:14:11 +0400

    add redis python

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import redis
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    db = get_redis_db()
+    count = get_latest_count(db) + 1
+    save(db, "count", count)
+
+    return HttpResponse("Hello, world. You have visited this site %s time(s)" % count)
+
+def get_redis_db():
+    return redis.Redis(host='pythonapp_redis_1', port=6379, decode_responses=True)
+
+def get_latest_count(db):
+    return parseInt(db.get("count"), 0)
+
+def save(db, key, value):
+    db.set(key, value)
+
+def parseInt(valueStr, defaultValue):
+    try:
+        return int(valueStr)
+    except:
+        return defaultValue
